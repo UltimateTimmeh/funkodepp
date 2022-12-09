87dcaaabdeeadd26d4d0075200feaa9c82b2f071
UltimateTimmeh <UltimateTimmeh@users.noreply.github.com>
2022-12-09 11:32:31 +0100

    Finished first version of new type matchup game

--- funkodepp/TypeMatchupGrid/TypeMatchupGrid.cpp
+++ funkodepp/TypeMatchupGrid/TypeMatchupGrid.cpp
@@ -21,19 +21,25 @@ public:
 private:
 
   // Grid settings.
-  int grid_x = 140;
-  int grid_y = 60;
-  int cell_size = 20;
+  int cell_size = 28;
+  int grid_x = (800-18*cell_size-80)/2+80;
+  int grid_y = (600-18*cell_size-80)/2+80;
 
-  // Cell states.
+  // Possible cell states.
   std::string cell_states[4] = { "0.0x", "0.5x", "1.0x", "2.0x" };
 
   // Default order of types.
-  std::string default_order[18] = {
+  std::string types_default_order[18] = {
     "Normal", "Fighting", "Flying", "Poison", "Ground", "Rock",
     "Bug", "Ghost", "Steel", "Fire", "Water", "Grass",
     "Electric", "Psychic", "Ice", "Dragon", "Dark", "Fairy"
   };
+
+  // Sprite that holds the type images
+  olc::Sprite *types_row = nullptr;
+  olc::Sprite *types_col = nullptr;
+  int sprite_h = cell_size;
+  int sprite_w = 80;
 
   // Default type matchup grid.
   int default_grid[18][18] = {
@@ -58,12 +64,19 @@ private:
     {2, 3, 2, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 3, 3, 2}, // Fairy
   };
 
-  // Game type matchup grid.
+  // Game grid.
   int game_grid[18][18];
+
+  // Variable for keeping score.
+  int score = 0;
 
 public:
   bool OnUserCreate() override
   {
+    // Load the type images sprites.
+    types_row = new olc::Sprite("types_row.png");
+    types_col = new olc::Sprite("types_col.png");
+
     // Populate the game grid.
     for (int r = 0; r < 18; ++r)
       for (int c = 0; c < 18; ++c)
@@ -103,8 +116,22 @@ public:
       }
     }
 
+    // Compute the current score.
+    score = 0;
+    for (int r = 0; r < 18; ++r)
+    {
+      for (int c = 0; c < 18; ++c)
+      {
+        int game_state = game_grid[r][c];
+        int expected_state = default_grid[r][c];
+        if (game_state == expected_state)
+          score += 1;
+      }
+    }
+
     // Draw all the things.
-    Clear(olc::BLACK);
+    olc::Pixel bg_color = { 42, 48, 62 };
+    Clear(bg_color);
 
     // Draw the game grid.
     for (int r = 0; r < 18; ++r)
@@ -131,9 +158,27 @@ public:
         int cell_x = c*cell_size + grid_x;
         int cell_y = r*cell_size + grid_y;
         FillRect(cell_x, cell_y, cell_size, cell_size, cell_color);
-        DrawRect(cell_x, cell_y, cell_size, cell_size, olc::BLACK);
+        DrawRect(cell_x, cell_y, cell_size, cell_size, bg_color);
       }
     }
+
+    // Draw the row header types.
+    int pos_x = grid_x - 80;
+    for (int i = 0; i < 18; ++i){
+      int pos_y = grid_y + i*sprite_h;
+      DrawPartialSprite(pos_x, pos_y, types_row, 0, i*sprite_h, sprite_w, sprite_h);
+    }
+
+    // Draw the column header types.
+    int pos_y = grid_y - 80;
+    for (int i = 0; i < 18; ++i){
+      int pos_x = grid_x + i*sprite_h;
+      DrawPartialSprite(pos_x, pos_y, types_col, i*sprite_h, 0, sprite_h, sprite_w);
+    }
+
+    // Draw the score.
+    DrawString(grid_x-65, grid_y-50, std::to_string(score), olc::WHITE, 2);
+    DrawString(grid_x-45, grid_y-30, "/"+std::to_string(18*18), olc::WHITE, 1);
 
     // Highlight the currently selected cell.
     if (cell_selected){
@@ -142,18 +187,18 @@ public:
       SetPixelMode(olc::Pixel::ALPHA);
       FillRect(cell_x, cell_y, cell_size, cell_size, { 0, 0, 0, 64 });
       SetPixelMode(olc::Pixel::NORMAL);
-      DrawRect(cell_x, cell_y, cell_size, cell_size, olc::BLACK);
+      DrawRect(cell_x, cell_y, cell_size, cell_size, bg_color);
     }
-
 
     return true;
   }
 };
 
+
 int main()
 {
   olcTypeMatchupGrid game;
-  if (game.Construct(640, 480, 1, 1))
+  if (game.Construct(800, 600, 1, 1))
     game.Start();
   return 0;
 }
